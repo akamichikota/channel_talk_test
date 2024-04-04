1d328f2aed29c134da2cb7f6bd0b7057ce2471fb
akamichikota <144325996+akamichikota@users.noreply.github.com>
2024-04-04 18:06:01 +0900

    Add files via upload

--- chat_test/registerUser.php
+++ chat_test/registerUser.php
@@ -1,22 +1,28 @@
 <?php
 $apiKey = 'a7981f14fe51e2b6e0d3d0368087e47a'; // Channel TalkのAPIキー
 
-// ユーザー情報
-$userInfo = array(
-    'userId' => 'user1', // ユニークなユーザーID
-    'name' => '赤道洸太', // ユーザー名
-    'email' => 'akamichi@gmail.com' // メールアドレス
-);
+// PHPのinputストリームからJSONデータを受け取る
+$data = json_decode(file_get_contents('php://input'), true);
 
-$url = 'https://api.channel.io/open/v3/users/upsert'; // ユーザー情報更新APIエンドポイント
+$userId = $data['userId'];
+$name = $data['name'];
+$email = $data['email'];
 
+// Channel Talkのユーザー情報更新APIエンドポイント
+$url = 'https://api.channel.io/open/v3/users/upsert';
+
+// cURLを使用してChannel Talk APIにリクエストを送信
 $ch = curl_init($url);
 curl_setopt($ch, CURLOPT_HTTPHEADER, array(
     'Content-Type: application/json',
     'X-Access-Token: ' . $apiKey
 ));
 curl_setopt($ch, CURLOPT_POST, true);
-curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($userInfo));
+curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode(array(
+    'userId' => $userId,
+    'name' => $name,
+    'email' => $email
+)));
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
 $response = curl_exec($ch);
@@ -24,5 +30,7 @@ if (!$response) {
     die('Error: "' . curl_error($ch) . '" - Code: ' . curl_errno($ch));
 }
 
-echo "Response: " . $response;
+// レスポンスを確認
+echo $response;
+
 curl_close($ch);
